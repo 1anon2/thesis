26f25a297d2e4b89be5d6f14ae5308ceaf10fa2f
flavi <flavi@FLAVIO.local>
2023-10-28 19:59:04 +0200

    Update requirements.txt and test_cannarella.py

--- test_cannarella.py
+++ test_cannarella.py
@@ -1,8 +1,9 @@
 import pickle
 
 
-# "../../thesis/MulTweEmo/datasePkl/df_no_duplicated_with_path2.pkl"
+# "../../thesis/MulTweEmo/datasetPkl/df_no_duplicated_with_path2.pkl"
 
 if __name__ == '__main__':
-    favorite_color = pickle.load(open("MulTweEmo/datasePkl/df_no_duplicated_with_path2.pkl", "rb"))
+    favorite_color = pickle.load(open("MulTweEmo/datasetPkl/df_no_duplicated_with_path2.pkl", "rb"))
     x=1
+    print('hello')
