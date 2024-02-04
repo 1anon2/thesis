2198f3662101ba4669c8240f7d854b2fa34c7219
flavio <flavio_rossi1@outlook.com>
2024-02-04 21:19:08 +0100

    First approach in Test_01

--- test_cannarella.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-
-# "../../thesis/MulTweEmo/datasetPkl/df_no_duplicated_with_path2.pkl"
-
-if __name__ == '__main__':
-    favorite_color = pickle.load(open("MulTweEmo/datasetPkl/df_no_duplicated_with_path2.pkl", "rb"))
-    x=1
-
